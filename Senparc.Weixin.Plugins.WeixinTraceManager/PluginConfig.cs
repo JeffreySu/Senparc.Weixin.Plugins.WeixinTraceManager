ba2cd3518f7e53b4402c27451e550050b3ce1d16
Jeffrey Su <zsu@senparc.com>
2018-02-06 20:43:03 +0800

    modfy page

---
+++ Senparc.Weixin.Plugins.WeixinTraceManager/PluginConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Senparc.Weixin.Plugins.WeixinTraceManager
+{
+    /// <summary>
+    /// 设置
+    /// </summary>
+    public class PluginConfig
+    {
+        public const string PROJECT_NAME = "WeixinTraceManager";
+        public const string PROJECT_NAME_CN = "微信日志管理";
+        public const string PROJECT_SOURCE_URL = "https://github.com/JeffreySu/Senparc.Weixin.Plugins.WeixinTraceManager";
+
+    }
+}
